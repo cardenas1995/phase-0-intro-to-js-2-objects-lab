1403db4f84ebb5066f17964c252db1d29e6f53d9
Kevin <k.cardenas1795@gmail.com>
2024-01-19 15:49:40 -0500

    creates myfile.md

--- test/indexTest.js
+++ test/indexTest.js
@@ -68,3 +68,42 @@ describe('employees', function() {
     });
   });
 });
+
+
+const employee = {
+  name: 'John Doe',
+  age: 30,
+  position: 'Developer'
+};
+
+describe('employees', function() {
+ 
+});
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const updatedEmployee = { ...employee }; 
+
+  updatedEmployee[key] = value;
+
+  return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value; 
+
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const clonedEmployee = { ...employee }; 
+
+  delete clonedEmployee[key]; 
+
+  return clonedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key]; 
+
+  return employee;
+}
